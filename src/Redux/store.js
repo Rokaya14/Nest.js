d228e7c31e2b91e10efbe5e7358e326774206ded
Rokaya <rokaiamagdi@gmail.com>
2021-09-26 22:10:45 +0200

    add redux-saga middleware

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,15 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import userReducer from "./ducks/counter";
 import counterReducer from "./ducks/counter";
-
+import createSagaMiddleware from "redux-saga";
 const reducer = combineReducers({
   counter: counterReducer,
   user: userReducer,
 });
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+const middleWare = [sagaMiddleware];
+
+const store = createStore(reducer, {}, applyMiddleware(...middleWare));
 
 export default store;
