287b1b50399d8eba58ab64393c4c9c96fe498f28
Rokaya <rokaiamagdi@gmail.com>
2021-09-27 00:03:58 +0200

    run saga middle ware

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,6 +2,7 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import userReducer from "./ducks/counter";
 import counterReducer from "./ducks/counter";
 import createSagaMiddleware from "redux-saga";
+import { watcherSaga } from "./sagas/rootSaga";
 const reducer = combineReducers({
   counter: counterReducer,
   user: userReducer,
@@ -11,5 +12,6 @@ const sagaMiddleware = createSagaMiddleware();
 const middleWare = [sagaMiddleware];
 
 const store = createStore(reducer, {}, applyMiddleware(...middleWare));
+sagaMiddleware.run(watcherSaga);
 
 export default store;
