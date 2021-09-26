c77046eeaef16fc9ce27cd78146671bb4ae8d224
Rokaya <rokaiamagdi@gmail.com>
2021-09-26 16:22:59 +0200

    create store and reducer

---
+++ src/Redux/ducks/counter.js
@@ -0,0 +1,29 @@
+//actions type
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+// actions creator function that return an object that contain the action
+export const increment = () => ({
+  type: INCREMENT,
+});
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+const initialState = {
+  count: 0,
+};
+
+// reducer
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+
+    default:
+      return state;
+  }
+};
+export default counterReducer;
