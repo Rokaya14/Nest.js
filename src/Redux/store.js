4f71c0b32e135cf9a6a3d3edfa338a89fe86e6c5
Rokaya <rokaiamagdi@gmail.com>
2021-09-27 15:11:27 +0200

    use redux-devtools-extension

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,15 +3,20 @@ import userReducer from "./ducks/counter";
 import counterReducer from "./ducks/counter";
 import createSagaMiddleware from "redux-saga";
 import { watcherSaga } from "./sagas/rootSaga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 const reducer = combineReducers({
   counter: counterReducer,
   user: userReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware();
-const middleWare = [sagaMiddleware];
 
-const store = createStore(reducer, {}, applyMiddleware(...middleWare));
+const store = createStore(
+  reducer,
+  {},
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(watcherSaga);
 
 export default store;
