779a07aca7ec11fc3e116edf0f63acdae9f046b0
Rokaya <rokaiamagdi@gmail.com>
2021-09-27 15:16:37 +0200

    dispatch getUser action that watcher will catch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./style.css";
 import Counter from "./Counter";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUser } from "./Redux/ducks/user";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
   const count = useSelector((state) => state.counter.count);
 
   const voters = [
