9ab9e1c192ec90445238113da40d5ce77413f4d3
Rokaya <rokaiamagdi@gmail.com>
2021-10-12 11:22:22 +0200

    create several  HTTP requests

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,6 +1,6 @@
 export class CreateItemDto {
   // readonly from outside and cann't be changed
   readonly name: string;
-  readonly descripton: string;
+  readonly description: string;
   readonly qty: number;
 }
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -7,8 +16,24 @@ export class ItemsController {
     return 'get all items';
   }
 
+  @Get(':id')
+  findone(@Param('id') id): string {
+    return `item is ${id}`;
+  }
+
   @Post()
-  create(): string {
-    return 'create items';
+  create(@Body() createItemDto: CreateItemDto): string {
+    const { name, description, qty } = createItemDto;
+    return `Name is  ${name}, Desc is ${description}, qty is ${qty}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete item that id is ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `update item that id is ${id} - Name is ${updateItemDto.name} `;
   }
 }
