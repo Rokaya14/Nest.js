552665606294d8bc8727d4372b353f418c9376d9
Rokaya <rokaiamagdi@gmail.com>
2021-10-17 11:19:47 +0200

    edit files

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -10,6 +10,7 @@ import {
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemsService } from './items.service';
 import { Item } from './interfaces/item.interface';
+
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemService: ItemsService) {}
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -32,16 +32,20 @@ export class ItemsService {
   async findAll(): Promise<Item[]> {
     return await this.itemModel.find();
   }
+
   async findOne(id: string): Promise<Item> {
     return await this.itemModel.findOne({ _id: id });
   }
+
   async create(item: Item): Promise<Item> {
     const newItem = new this.itemModel(item);
     return await newItem.save();
   }
+
   async delete(id: string): Promise<Item> {
     return await this.itemModel.findByIdAndRemove(id);
   }
+
   async update(id: string, item: Item): Promise<Item> {
     return await this.itemModel.findByIdAndUpdate(id, item, { new: true });
   }
